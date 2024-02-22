15ae0ef6e98b417b6eccbbf60a73c83bb71b0aec
Adithya <jhadith88@gmail.com>
2024-02-22 13:19:18 +0530

    ghg

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,15 @@
-import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../../firebase/clientApp";
-import useAuth from "../../hooks/useAuth";
-import Navbar from "../Navbar";
-import AuthModal from "../Modal/Auth";
+import React, { ReactNode } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../firebase/clientApp';
+import useAuth from '../../hooks/useAuth';
+import Navbar from '../Navbar';
+import AuthModal from '../Modal/Auth';
 
-const Layout: React.FC = ({ children }) => {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   // useAuth(); // will implement later at end of tutorial
 
   return (
